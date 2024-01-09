637d7c534971ca32c03c271d3225e27983e8697f
Hendra Afrizal <hendraafrizal30@gmail.com>
2024-01-09 23:07:57 +0700

    .

--- routes/custom/superadmin.php
+++ routes/custom/superadmin.php
@@ -5,18 +5,4 @@ use App\Http\Controllers\Superadmin\RolesController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('superadmin')->name('superadmin.')->middleware(['auth', 'permission:akses_menu_superadmin'])->group(function () {
-
-    // ROLES
-    Route::resource('roles', RolesController::class)->except(['create', 'show'])->names([
-        'index' => 'kelola-pengguna.roles.index',
-    ]);
-    // End ROLES
-
-    // ROLE PERMISSIONS
-    Route::resource('role-permissions', RolePermissionsController::class)->except(['create', 'show'])->names([
-        'index' => 'kelola-pengguna.role-permissions.index',
-    ]);
-    Route::post('role-permissions/group-access', [RolePermissionsController::class, 'groupAccess'])->name('role-permissions.group-access');
-    // End ROLE PERMISSIONS
-
 });
