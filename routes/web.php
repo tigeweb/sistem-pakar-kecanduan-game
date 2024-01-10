fec75c947b5a39cd0f6beea3bf41f4026f58ebd5
Hendra Afrizal <hendraafrizal30@gmail.com>
2024-01-10 09:44:38 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('login'));
+    return redirect(route('diagnosa.index'));
 });
 
 // Akses Admin
